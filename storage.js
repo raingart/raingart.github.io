f4f062bc3af9b66a7c7dea54d86b0b281ab77624
raingart <13064767+raingart@users.noreply.github.com>
2021-02-18 12:56:31 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,8 +1,11 @@
 const Storage = {
-   setParams: x => localStorage.setItem('store', JSON.stringify(x)),
-
-   getParams(callback, sync_type, x) {
+   //setParams: x => localStorage.setItem('store', JSON.stringify(x)),
+   
+   /*getParams(callback, sync_type, x) {
       const data = JSON.parse(localStorage.getItem('store'));
       callback(x ? data?.x : data);
-   },
+   },*/
+               
+   setParams() {},
+   getParams() {},
 };
