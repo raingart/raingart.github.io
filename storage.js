68259d4074c1a85af927d840a208b38ac179ff1b
raingart <13064767+raingart@users.noreply.github.com>
2021-02-18 09:44:07 +0200

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,13 @@
+const Storage = function () {
+   let saveParams = {};
+
+   return {
+      setParams(x = required(), sync_type) {
+         
+      },
+
+      getParams(callback, sync_type, x) {
+         
+      },
+   }
+}();
