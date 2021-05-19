1283b815a803337586e1d3a27d475834ca41c4ce
raingart <13064767+raingart@users.noreply.github.com>
2021-05-19 21:59:57 +0300

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -42,7 +42,7 @@ const Plugins = {
    load(list) {
       (list || this.list).forEach(plugin => {
          try {
-            this.injectScript('https://ghcdn.rawgit.org/raingart/Nova-YouTube-extension/master/plugins/' + plugin);
+            this.injectScript('https://raw.githack.com/raingart/Nova-YouTube-extension/master/plugins/' + plugin);
          } catch (error) {
             console.error(`plugin loading failed: ${plugin}\n${error.stack}`);
          }
