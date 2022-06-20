e3587ef27a51ac5ece2d898fd81f949f21983cba
raingart <13064767+raingart@users.noreply.github.com>
2022-06-20 14:55:56 +0300

    Update options.js

--- js/nova/options.js
+++ js/nova/options.js
@@ -1,10 +1,10 @@
 console.debug('init options patch.js');
 
 Conf.init = settings => {
-   console.debug('Conf.init', settings);
    if (settings) PopulateForm.fill(settings);
    Conf.attrDependencies();
    Conf.registerEventListener();
+   document.body.classList.remove('preload');
    // auto selects value on focus
    document.body.querySelectorAll('form input[type]').forEach(i => i.addEventListener('focus', i.select));
    Conf.btnSubmitAnimation.submitBtns = document.body.querySelectorAll('form [type=submit]');
