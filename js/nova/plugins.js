7d0572931bea504ec6fcd6e7f935a673e4f020c2
raingart <13064767+raingart@users.noreply.github.com>
2022-06-21 10:09:35 +0300

    Update plugins.js

--- js/nova/plugins.js
+++ js/nova/plugins.js
@@ -1,7 +1,7 @@
 Plugins.load = function(list) {
    (list || this.list).forEach(plugin => {
       try {
-         this.injectScript('https://raw.githubusercontent.com/raingart/Nova-YouTube-extension/master/plugins/' + plugin);
+         this.injectScript('https://raw.githack.com/raingart/Nova-YouTube-extension/master/plugins/' + plugin);
       } catch (error) {
          console.error(`plugin loading failed: ${plugin}\n${error.stack}`);
       }
