d8913e89e08b78223c102fbe92e277d09c739d76
raingart <13064767+raingart@users.noreply.github.com>
2022-06-24 07:45:41 +0300

    Update storage.js

--- js/nova/storage.js
+++ js/nova/storage.js
@@ -11,13 +11,3 @@ const Storage = {
    setParams: x => {},
    getParams: callback => callback(storeData),
 };
-
-/*Conf.init = function() {
-   PopulateForm.fill(storeData);
-   this.attrDependencies();
-   this.registerEventListener();
-   //document.body.classList.remove('preload'); // should be hidden on the userscript side
-   // auto selects value on focus
-   document.body.querySelectorAll('form input[type]').forEach(i => i.addEventListener('focus', i.select));
-   this.btnSubmitAnimation.submitBtns = document.body.querySelectorAll('form [type=submit]');
-};*/
