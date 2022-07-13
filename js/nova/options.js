8323212273a93c505561d8f68fcb9a4277c1d706
raingart <13064767+raingart@users.noreply.github.com>
2022-07-13 09:38:58 +0300

    Update options.js

--- js/nova/options.js
+++ js/nova/options.js
@@ -1,11 +1,13 @@
 console.debug('init options patch.js');
 
 Conf.init = function() {
-   PopulateForm.fill(storeData);
-   this.attrDependencies();
-   this.registerEventListener();
-   //document.body.classList.remove('preload'); // should be hidden on the userscript side
-   // auto selects value on focus
-   document.body.querySelectorAll('form input[type]').forEach(i => i.addEventListener('focus', i.select));
-   this.btnSubmitAnimation.submitBtns = document.body.querySelectorAll('form [type=submit]');
+   Storage.getParams(settings => {
+      PopulateForm.fill(storeData);
+      this.attrDependencies();
+      this.registerEventListener();
+      //document.body.classList.remove('preload'); // should be hidden on the userscript side
+      // auto selects value on focus
+      document.body.querySelectorAll('form input[type]').forEach(i => i.addEventListener('focus', i.select));
+      this.btnSubmitAnimation.submitBtns = document.body.querySelectorAll('form [type=submit]');
+   }, this.storageMethod);
 };
