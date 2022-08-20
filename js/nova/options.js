f72133c80252f005eb5340713bdfd6973ccb4a10
raingart <13064767+raingart@users.noreply.github.com>
2022-08-20 21:58:47 +0300

    Update options.js

--- js/nova/options.js
+++ js/nova/options.js
@@ -11,3 +11,19 @@ Conf.init = function() {
       this.btnSubmitAnimation.submitBtns = document.body.querySelectorAll('form [type=submit]');
    }, this.storageMethod);
 };
+
+document.addEventListener('submit', evt => {
+   let newSettings = {};
+
+   for (let [key, value] of new FormData(form)) {
+      if (newSettings.hasOwnProperty(key)) { // SerializedArray
+         newSettings[key] += ',' + value; // add new
+         newSettings[key] = newSettings[key].split(','); // to array [old, new]
+
+      } else {
+         newSettings[key] = value;
+      };
+   }
+
+   console.log(newSettings);
+});
