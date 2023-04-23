3caf951b46bd4b0a9908f1d371c474767205f0af
raingart <13064767+raingart@users.noreply.github.com>
2023-04-23 15:14:35 +0300

    Update plugins.js

--- js/nova/plugins.js
+++ js/nova/plugins.js
@@ -1,9 +1,11 @@
-Plugins.load = function(list) {
-   (list || this.list).forEach(plugin => {
-      try {
-         this.injectScript('https://raw.githack.com/raingart/Nova-YouTube-extension/master/plugins/' + plugin);
-      } catch (error) {
-         console.error(`plugin loading failed: ${plugin}\n${error.stack}`);
-      }
-   })
+Plugins.load = function (list) {
+   (list || this.list)
+      .filter(n => !n.startsWith('+'))
+      .forEach(plugin => {
+         try {
+            this.injectScript('https://raw.githack.com/raingart/Nova-YouTube-extension/master/plugins/' + plugin);
+         } catch (error) {
+            console.error(`plugin loading failed: ${plugin}\n${error.stack}`);
+         }
+      })
 }
