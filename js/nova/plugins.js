4bc70ea31afe188623c2f2a3a9c023a2d68bc46e
raingart <13064767+raingart@users.noreply.github.com>
2023-06-21 20:44:03 +0300

    Update plugins.js

--- js/nova/plugins.js
+++ js/nova/plugins.js
@@ -1,10 +1,6 @@
 
 if (location.hash == '#demo') {
-      const script = document.createElement('script');
-      script.src = 'https://raw.githack.com/raingart/Nova-YouTube-extension/master/js/plugins.js';
-      script.defer = true; // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-defer:~:text=defer,-This
-      script.async = true;
-      (document.head || document.documentElement).append(script);
+      const script = injectScript('https://raw.githack.com/raingart/Nova-YouTube-extension/master/js/plugins.js');
 
       script.onload = () => {
             alert(1)
@@ -26,3 +22,15 @@ else {
       load(){},
    }
 }
+
+injectScript('https://raw.githack.com/raingart/Nova-YouTube-extension/master/js/optionsBilder.js');
+
+function injectScript(source = required()) {
+      const script = document.createElement('script');
+      script.src = source;
+      script.defer = true; // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-defer:~:text=defer,-This
+      script.async = true;
+      (document.head || document.documentElement).append(script);
+      return script;
+}
+
